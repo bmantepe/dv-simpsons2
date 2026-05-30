77c4e33fee85d587bbd00da9c3db4a8a3e4c412e
Biel Manté <bmantepe@gmail.com>
2026-05-31 01:00:11 +0200

    dasboard guapo

--- src/dashboard.py
+++ src/dashboard.py
@@ -5,7 +5,9 @@ import streamlit.components.v1 as components
 
 # Import functions from your modules
 #from queries.q1_q5 import create_character_plot, load_data_q1_q5
-from queries.q1_q5_v2 import create_plot_q1_q5_html
+from queries.q1_q5 import create_plot_q1_q5_html
+from queries.q2_v2 import create_plot_q2_html
+
 # from queries.q2 import create_timeline_plot, load_data_q2
 # from queries.q3_q4 import create_diverging_difference_plot, create_episode_comparison_plot, load_data_q3_q4
 from queries.q3_q4 import create_plot_q4_html
@@ -23,26 +25,10 @@ st.set_page_config(layout="wide", page_title="The Simpsons Dialogue Dashboard")
 st.header("1. Character Dialogue Distribution")
 st.markdown("Click on a character's bar on the left to **filter** their specific dialogue distribution on the right. Shift-click to select multiple characters.")
 
-word_or_sentence = st.radio(
-    "View by",
-    options=["Word", "Sentence"],
-    format_func=lambda v: "Word Count" if v == "Word" else "Sentence Count",
-    horizontal=True,
-)
-
-map = {"Word": "word_count", "Sentence": "sentence_count"}
-view = map[word_or_sentence]
-
-html = create_plot_q1_q5_html(view)
-components.html(html, height=700, scrolling=True)
 
 
-st.divider()
-
-
-
-st.header("2. Dialogue Timeline Analysis")
-st.markdown("Select characters from the dropdown below to track their word counts over the seasons. Hover over the points to see specific values and character images.")
+html = create_plot_q1_q5_html()
+components.html(html, height=600, scrolling=False)
 
 
 st.divider()
@@ -50,7 +36,8 @@ st.divider()
 
 
 
-st.header("1. Character Dialogue Distribution My version")
+
+st.header("3. Character Word By Episode Distribution")
 
 view = st.radio(
     "Chart view",
